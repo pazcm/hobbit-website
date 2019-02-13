33a689a14fbba4edeb919456641e5b62acd35c78
Marta Paz <pazmarta@gmail.com>
2019-02-13 12:28:52 +0000

    create routes to pages

--- run.py
+++ run.py
@@ -1,18 +1,23 @@
-"""
-Render HTML code
-"""
 import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
+"""
+Routing: Allows us to swith between views using URLs 
+create a route and a view
+"""
 @app.route('/')
-def hello():
-    # return "Hello Flask"
+def index():
     return render_template("index.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=True)
+debug=True)
