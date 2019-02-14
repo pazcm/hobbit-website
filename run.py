a669e7274d0bbc812e29de8de50de8fb20d420f9
Marta Paz <pazmarta@gmail.com>
2019-02-14 16:44:21 +0000

    add advance routing

--- run.py
+++ run.py
@@ -1,41 +1,46 @@
 import os
-import json 
+import json
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-"""
-Routing: Allows us to switch between views using URLs 
-create another template and another route
-use Jinja templating
 
-"""
 @app.route('/')
 def index():
     return render_template("index.html")
 
-"""
-set data on the server side and get it to come through to the client
-"""
+
 @app.route('/about')
 def about():
     data = []
     with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
-    return render_template("about.html", page_title="About", company_data=data)
-   
+    return render_template("about.html", page_title="About", company=data)
+
+
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+    
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+    
+    return render_template("member.html", member=member)
+
 
 @app.route('/contact')
 def contact():
     return render_template("contact.html", page_title="Contact")
 
 
-@app.route('/carees.html')
-def carees():
-    return render_template("carees.html", page_title="Carees")
-
+@app.route('/careers')
+def careers():
+    return render_template("careers.html", page_title="Careers")
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
-debug=True)
+        port=int(os.environ.get('PORT')),
+        debug=True)
